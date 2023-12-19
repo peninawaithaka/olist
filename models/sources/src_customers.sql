d1d1ce7b90ae4711f474315521d024bd7e9df542
Penina Waithaka <peninawaithaka5@gmail.com>
2023-12-19 12:13:24 +0300

    staging customer data

---
+++ models/sources/src_customers.sql
@@ -0,0 +1,16 @@
+-- fetch all source data from raw_customers dataset where ids are not null
+-- remove the "" in varchar ids
+
+with raw_customers as (
+
+    select * from {{ source('olist', 'raw_customers_dataset')}}
+)
+
+select 
+    replace(customer_id, '"', '') as customer_id,
+    replace(customer_unique_id, '"', '') as customer_unique_id,
+    replace(customer_zip_code_prefix, '"', '') as customer_zip_code_prefix,
+    customer_city,
+    customer_state
+from raw_customers
+where customer_id is not null and customer_unique_id is not null
